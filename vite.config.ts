2cdf06168905450112d1f11182bdd8ac99845218
khushi <khushivasava8@gmail.com>
2026-06-19 21:17:43 +0530

    config: set base path to root for Vercel compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Hybrid-Intrusion-Detection-System-using-CICIDS-2017-Dataset/',
+  base: '/',
 })
